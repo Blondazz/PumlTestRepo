2698b6d3dab2c99f1431549e335fb5d450267e64
Gazda99 <jakub.gazdzik@outlook.com>
2022-12-05 12:58:39 +0100

    XX

--- PumlTestRepo/Class1.cs
+++ PumlTestRepo/Class1.cs
@@ -1,9 +1,9 @@
-﻿namespace PumlTestRepo;
+﻿using System.Security;
+
+namespace PumlTestRepo;
 
 public class C1
 {
-
-    
     private int intPrivate;
     public int IntPublic;
     private const int IntConst = 10;
@@ -12,20 +12,45 @@ public class C1
     public static int IntStaticPublic = 40;
     protected int IntProtected = 50;
     internal int IntInternal = 55;
+    protected internal int IntInternalProtected = 55;
+    protected internal int IntInternalProtectedWithoutValue;
+
+    internal void VoidInternal()
+    {
+    }
+
+    protected void VoidProtected(int a, InternalBufferOverflowException b, SuppressUnmanagedCodeSecurityAttribute c)
+    {
+    }
+
+    protected internal void VoidProtectedInternal()
+    {
+    }
 
     private int IntProp { get; set; }
     public int IntPropPublic { get; set; } = 60;
 
     public int IntPropCombined1 { private get; init; }
 
-    public void Void(){}
-    
-    private void VoidPrivate(){}
-    
-    public void VoidWithArgs(string args, int args2){}
-    
-    public static void VoidStaticPublic(){}
-    private static void VoidStaticPrivate(List<Type> a){}
+    public void Void()
+    {
+    }
+
+    private void VoidPrivate()
+    {
+    }
+
+    public void VoidWithArgs(string args, int args2)
+    {
+    }
+
+    public static void VoidStaticPublic()
+    {
+    }
+
+    private static void VoidStaticPrivate(List<Type> a)
+    {
+    }
 
     public List<string> ListPublic()
     {
@@ -34,19 +59,22 @@ public class C1
 
     public C1()
     {
-        
-    }
-    
-    public C1(C1Interface c1Interface)
-    {
-        
     }
 
+    public C1(C1Interface c1Interface)
+    {
+    }
 }
 
 public interface C1Interface
 {
-    
+}
+
+public abstract class AbstractClass
+{
+    public abstract void AVoid();
+    public abstract int X { get; set; }
+    public abstract DateOnly DDD(DateTime someName);
 }
 
 public struct C2
@@ -67,6 +95,4 @@ public static class C4
 public class C5<T1, T2>
 {
     public T1 T1Field;
-    
-    
 }
