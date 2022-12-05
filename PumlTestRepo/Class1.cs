bac5837c5776c803f636ac3daed94228d555e558
Gazda99 <jakub.gazdzik@outlook.com>
2022-12-05 12:32:49 +0100

    super

--- PumlTestRepo/Class1.cs
+++ PumlTestRepo/Class1.cs
@@ -1,6 +1,63 @@
 ﻿namespace PumlTestRepo;
 
-public class Class1
+public class C1
+{
+
+    
+    private int intPrivate;
+    public int IntPublic;
+    private const int IntConst = 10;
+    public const int IntConstPublic = 20;
+    private static int IntStatic;
+    public static int IntStaticPublic = 40;
+
+    private int IntProp { get; set; }
+    public int IntPropPublic { get; set; } = 60;
+
+    public int IntPropCombined1 { private get; init; }
+
+    public void Void(){}
+    
+    private void VoidPrivate(){}
+    
+    public void VoidWithArgs(string args, int args2){}
+    
+    public static void VoidStaticPublic(){}
+    private static void VoidStaticPrivate(List<Type> a){}
+
+    public List<string> ListPublic()
+    {
+        return null;
+    }
+
+    public C1()
+    {
+        
+    }
+    
+    public C1(C1Interface c1Interface)
+    {
+        
+    }
+
+}
+
+public interface C1Interface
 {
     
+}
+
+public struct C2
+{
+}
+
+internal class C3
+{
+    private class C3Inside
+    {
+    }
+}
+
+public static class C4
+{
 }
